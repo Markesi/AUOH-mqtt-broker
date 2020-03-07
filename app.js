e2694f367d50396d81da95a0a7a9839ba115c204
MichelangeloMarchesi <michlangelo.marchesi@seamk.fi>
2020-03-07 13:47:07 +0200

    note on PORT uppercase

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const aedes = require('aedes')()
 const httpServer = require('http').createServer()
 const ws = require('websocket-stream')
 // defines a listening process on port among the 65535 availables
+// HAS TO BE WRITEN uppercas -> PORT
 const port = process.env.PORT || 8081;
 
 ws.createServer({ 
